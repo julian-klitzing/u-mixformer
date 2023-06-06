cb6e3949b9b29bc4771acd3fff05a68728982232
Seul-Ki Yeom <phoenixysk@gmail.com>
2023-06-06 07:51:33 +0000

    config file change

--- configs/feedformer/B0/feedformer.b0.512x512.ade.160k.py
+++ configs/feedformer/B0/feedformer.b0.512x512.ade.160k.py
@@ -1,6 +1,6 @@
 _base_ = [
     '../../_base_/models/segformer_mit-b0.py', '../../_base_/datasets/ade20k.py',
-    '../../_base_/default_runtime.py', '../../_base_/schedules/schedule_160k_adamw.py'
+    '../../_base_/default_runtime.py', '../../_base_/schedules/schedule_160k.py'
 ]
 crop_size = (512, 512)
 data_preprocessor = dict(size=crop_size)
@@ -41,6 +41,6 @@ param_scheduler = [
         by_epoch=False,
     )
 ]
-train_dataloader = dict(batch_size=2, num_workers=2)
-val_dataloader = dict(batch_size=1, num_workers=4)
+train_dataloader = dict(batch_size=32, num_workers=4)
+val_dataloader = dict(batch_size=16, num_workers=4)
 test_dataloader = val_dataloader
