a6e39e0813cbd4fdc84bd1ce9686e31ae18b7076
julian-klitzing <julian.klitzing@web.de>
2023-07-24 09:02:20 +0000

    new version of MiT that returns inital patch embed

--- mmseg/models/backbones/__init__.py
+++ mmseg/models/backbones/__init__.py
@@ -27,12 +27,13 @@ from .mit_eca import MixVisionTransformer_new
 from .mit import EfficientMultiheadAttention
 from .mit_btn import MixVisionTransformer_btn
 from .mit_btn2 import MixVisionTransformer_btn2
+from .mit_pe import MixVisionTransformerPE
 
 __all__ = [
     'ResNet', 'ResNetV1c', 'ResNetV1d', 'ResNeXt', 'HRNet', 'FastSCNN',
     'ResNeSt', 'MobileNetV2', 'UNet', 'CGNet', 'MobileNetV3',
     'VisionTransformer', 'SwinTransformer', 'MixVisionTransformer', 'MixVisionTransformer_new', 'MixVisionTransformer_btn', 'MixVisionTransformer_btn2',
     'BiSeNetV1', 'BiSeNetV2', 'ICNet', 'TIMMBackbone', 'ERFNet', 'PCPVT',
-    'SVT', 'STDCNet', 'STDCContextPathNet', 'BEiT', 'MAE', 'PIDNet', 'MSCAN',
+    'SVT', 'STDCNet', 'STDCContextPathNet', 'BEiT', 'MAE', 'PIDNet', 'MSCAN', 'MixVisionTransformerPE'
     # 'mit_b0', 'mit_b1', 'mit_b2', 'mit_b3', 'mit_b4', 'mit_b5'
 ]
